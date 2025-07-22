c687044e2c1da62bc545e189862c1dd533fd5ab0
Shin-Yun-Cheol <yptlsdbscjf@naver.com>
2025-07-22 16:51:48 +0900

    refactor : Application.java Factory 패턴 적용
    Application.java에서 Simple Factory 패턴을 이용해서 객체 생성을 분리했습니다.

---
+++ src/main/java/mission/factory/FishFactory.java
@@ -0,0 +1,24 @@
+package mission.factory;
+
+import mission.model.Fish;
+
+import java.util.List;
+
+public class FishFactory {
+    public static List<Fish> createDefaultFishList() {
+        return List.of(
+                new Fish(1, "플랑크톤", 1),
+                new Fish(2, "멸치", 2),
+                new Fish(3, "정어리", 2),
+                new Fish(4, "빙어", 2),
+                new Fish(5, "고등어", 3),
+                new Fish(6, "전갱이", 3),
+                new Fish(7, "꽁치", 3),
+                new Fish(8, "삼치", 4),
+                new Fish(9, "참치", 4),
+                new Fish(10, "방어", 4),
+                new Fish(11, "황새치", 5),
+                new Fish(12, "상어", 5)
+        );
+    }
+}
