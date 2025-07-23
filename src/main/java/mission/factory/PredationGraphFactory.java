6dbf5988e873e3b0c121cea45da6906d0ec1b4a2
Shin-Yun-Cheol <yptlsdbscjf@naver.com>
2025-07-23 14:50:59 +0900

    refactor(PredationGraph) : 먹이사슬 관계를 enum으로 정의
    PredationGraphFactory.java에서 먹이사슬 관계를 하드코딩 하던 것을 enum을 이용해서 처리.

--- src/main/java/mission/factory/PredationGraphFactory.java
+++ src/main/java/mission/factory/PredationGraphFactory.java
@@ -1,23 +1,20 @@
 package mission.factory;
 
+import mission.model.PredationRelationship;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PredationGraphFactory {
     public static Map<Integer, List<Integer>> createDefaultGraph(){
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        graph.put(2, List.of(1));
-        graph.put(3, List.of(1));
-        graph.put(4, List.of(1));
-        graph.put(5, List.of(2, 3));
-        graph.put(6, List.of(3));
-        graph.put(7, List.of(2, 4));
-        graph.put(8, List.of(5, 6));
-        graph.put(9, List.of(5, 6, 7));
-        graph.put(10, List.of(6, 7));
-        graph.put(11, List.of(8, 10));
-        graph.put(12, List.of(9, 10));
-        return graph;
+        Map<Integer, List<Integer>> predationGraph = new HashMap<>();
+
+        for(PredationRelationship predationRelationship : PredationRelationship.values()){
+            if(!predationRelationship.getPreyIds().isEmpty()){
+                predationGraph.put(predationRelationship.getPreyIds().size(), predationRelationship.getPreyIds());
+            }
+        }
+        return predationGraph;
     }
 }
